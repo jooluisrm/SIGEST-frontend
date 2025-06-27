1ea7988f056534b16ea38d01e0c81bdc15b46192
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-06-27 12:03:07 -0300

    ui: aplicar estilo visual padrão na página de cadastro - Adição de imagem de fundo, overlay e efeito de blur para consistência visual - Inclusão da logo de fundo como marca d'água

--- src/app/(rotas)/cadastrar/[type]/page.tsx
+++ src/app/(rotas)/cadastrar/[type]/page.tsx
@@ -1,4 +1,5 @@
 import { MainCadastrar } from "@/components/cadastrar/mainCadastrar";
+import Image from "next/image";
 
 type Props = {
     params: {
@@ -8,8 +9,17 @@ type Props = {
 
 const Page = ({ params }: Props) => {
     return (
-        <section>
-            <MainCadastrar type={params.type}/>
+        <section className="bg-[url('/assets/bg-login.jpg')] bg-cover bg-center min-h-screen">
+            <div className="w-full h-screen bg-white/50 backdrop-blur relative">
+                <Image
+                    alt="logo sigest"
+                    src={'/assets/sigest-logo.png'}
+                    width={942}
+                    height={362}
+                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-50"
+                />
+                <MainCadastrar type={params.type} />
+            </div>
         </section>
     );
 }
