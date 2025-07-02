c55913e7aa4678a385f2afda7b8cae19eb1a65e9
Tiago <tiagobonifacio3@gmail.com>
2025-07-02 17:14:48 -0300

    feat: adicionada validação com Zod e botões de navegação no formulário
    - Integração com React Hook Form
    
    - Adicionados botões de 'Voltar' e 'Cadastrar'
    
    - Feedback visual de erro

---
+++ src/components/cadastrar/buttonCadastro.tsx
@@ -0,0 +1,18 @@
+import { ArrowLeft } from "lucide-react";
+import { Button } from "../ui/button";
+
+type Props = {
+    text: string;
+    onClick: () => void;
+}
+
+export const ButtonCadastro = ({ text,onClick }: Props) => {
+    return (
+        <Button onClick={onClick} variant={"default"} className="rounded-2xl bg-secundaria h-10 w-17 sm:h-12 sm:w-22 font-bold cursor-pointer ml-1 sm:ml-4">
+            
+        <ArrowLeft className="hidden sm:block"/>
+        {text}
+            
+        </Button>
+    );
+}
--- src/components/cadastrar/inputCadastro.tsx
+++ src/components/cadastrar/inputCadastro.tsx
@@ -1,9 +1,15 @@
 import { Input } from "../ui/input";
 
+type Props = React.InputHTMLAttributes<HTMLInputElement> & {
+  camp: string;
+};
 
-export const InputCadastro = () => {
-    return (
-        <Input 
-            className="rounded-2xl bg-primaria text-text1 border-0 placeholder:text-text1 placeholder:text-center h-12"/>
-    );
-}
+export const InputCadastro = ({ camp, ...props }: Props) => {
+  return (
+    <Input
+      type={camp}
+      className="rounded-2xl bg-primaria text-text1 border-0 placeholder:text-text1 h-12 px-4 appearance-none"
+      {...props}
+    />
+  );
+};
