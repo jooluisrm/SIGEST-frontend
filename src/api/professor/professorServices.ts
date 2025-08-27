b054851c38af4ce1ba4ad37b39488ee45d1162ef
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-08-27 09:45:24 -0300

    refactor: centralizar tipagens de professor em arquivo dedicado - Criação do arquivo professor.ts para agrupar os tipos da entidade - Movimentação do tipo TypeProfessorCadastro para o novo arquivo

--- src/api/professor/professorServices.ts
+++ src/api/professor/professorServices.ts
@@ -1,31 +1,11 @@
 import axiosInstance from "@/lib/axiosInstance";
+import { TypeProfessorCadastro } from "@/types/professor";
 
 export const getProfessores = async () => {
     const response = await axiosInstance.get('api/professors');
     console.log(response.data);
     return response.data;
 };
-
-export type TypeProfessorCadastro = {
-    nome: string;
-    data_nascimento: string; // formato ISO ou "YYYY-MM-DD"
-    cpf: string;
-    rg: string;
-    genero: string;
-    nome_pai: string;
-    nome_mae: string;
-    deficiencia?: string;
-    logradouro: string;
-    numero: string;
-    bairro: string;
-    complemento?: string;
-    cidade: string;
-    estado: string;
-    telefone: string;
-    celular: string;
-    email: string;
-    matricula_adpm?: string;
-}
 
 export const postCadastrarProfessor = async (data: TypeProfessorCadastro) => {
     const response = await axiosInstance.post('api/professors', data);
