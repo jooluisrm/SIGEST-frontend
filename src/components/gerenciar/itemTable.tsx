1f4d4d418d4d62d3b847cee1e810c149716d2f16
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-09-01 21:01:18 -0300

    feat: criar componente reutilizável para dialog de confirmação (alert) - Desenvolvimento do AlertDialogComponent para padronizar diálogos de confirmação - O componente utiliza o ButtonGerenciar como gatilho para a ação - Implementação da funcionalidade de 'Deletar' na tabela de gerenciamento com o novo componente

--- src/components/gerenciar/itemTable.tsx
+++ src/components/gerenciar/itemTable.tsx
@@ -2,6 +2,7 @@ import { TypeProfessorCadastro } from "@/types/professor";
 import { TableCell, TableRow } from "../ui/table";
 import { ActionDialog } from "./actionDialog";
 import { UserType } from "@/app/(rotas)/(privadas)/cadastrar/[type]/page";
+import { AlertDialogComponent } from "../shared/alertComponent";
 
 type Props = {
     type: UserType;
@@ -15,7 +16,14 @@ export const ItemTable = ({ type, item }: Props) => {
             <TableCell className="hidden md:table-cell">{item.email}</TableCell>
             <TableCell className="hidden md:table-cell">{item.telefone}</TableCell>
             <TableCell>
-                <div className="flex items-center justify-end gap-2">
+                <div className="flex items-center justify-end gap-1">
+                    <AlertDialogComponent
+                        triggerClassName="bg-red-500"
+                        triggerIcon="delete"
+                        triggerTooltip={`Deletar ${type}`}
+                        dialogTitle={`Deletar ${type}`}
+                        dialogDescription={`Tem certeza que deseja deletar este ${type}? Essa ação não poderá ser desfeita.`}
+                    />
                     <ActionDialog
                         triggerIcon="edit"
                         triggerTooltip={`Editar ${type}`}
