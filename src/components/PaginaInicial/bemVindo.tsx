e735c9fc23e64b6a8ba0e2660d5a707efccc044f
Tiago <tiagobonifacio3@gmail.com>
2025-09-24 15:14:30 -0300

    feat: criação do componente de imagem para o componente Bem Vindo

--- src/components/PaginaInicial/bemVindo.tsx
+++ src/components/PaginaInicial/bemVindo.tsx
@@ -1,21 +1,15 @@
-import Image from "next/image";
 import { Card } from "../ui/card";
+import { LapisBemVindo } from "./lapisBemVindo";
 
 export const BemVindo = () => {
   return (
     <Card className="relative w-full h-30 sm:h-50 md:h-70 rounded-md">
       <div className="absolute top-0 left-0">
-        <Image
-          src="/assets/lapis-icon.png"
-          width={100}
-          height={100}
-          className="sm:w-50 sm:h-20 md:w-80 md:h-25"
-          alt="Bem-Vindo"
-        />
+        <LapisBemVindo />
       </div>
 
       <div className="flex justify-center items-center h-full px-4">
-        <h1 className="text-2xl sm:text-4xl md:text-8xl font-bold text-[#008C35] text-center">
+        <h1 className="text-2xl sm:text-5xl md:text-7xl font-bold text-[#008C35] text-center">
           Bem Vindo
         </h1>
       </div>
