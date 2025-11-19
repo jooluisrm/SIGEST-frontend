d26ade50b5e07a8583da78cbab636f560a518a39
Tiago <tiagobonifacio3@gmail.com>
2025-11-19 16:38:52 -0300

    criacao do caso de uso consultar informacoes do aluno

--- src/components/gerenciar/itemTable.tsx
+++ src/components/gerenciar/itemTable.tsx
@@ -8,71 +8,71 @@ import { FormProfessor } from "../cadastrar/formularios/formProfessor";
 import { FormAluno } from "../cadastrar/formularios/formAluno";
 import { FormDisciplina } from "../cadastrar/formularios/formDisciplina";
 import { FormUsuario } from "../cadastrar/formularios/formUsuario";
+import { ItemView } from "./itemView";
 
 type Props = {
-    item: TypeProfessorCadastro | any;
-}
+  item: TypeProfessorCadastro | any;
+};
 
 export const ItemTable = ({ item }: Props) => {
+  const { type } = usePageType();
 
-    const { type } = usePageType();
+  let renderPageEdit: React.ReactNode = null;
 
-    let renderPageEdit: React.ReactNode = null;
+  switch (type) {
+    case "professor":
+      renderPageEdit = <FormProfessor isEdit={true} defaultValues={item} />;
+      break;
+    case "aluno":
+      renderPageEdit = <FormAluno isEdit={true} defaultValues={item} />;
+      break;
+    case "disciplina":
+      renderPageEdit = <FormDisciplina isEdit={true} defaultValues={item} />;
+      break;
+    case "usuario":
+      renderPageEdit = <FormUsuario isEdit={true} defaultValues={item} />;
+      break;
+  }
 
-    switch (type) {
-        case "professor":
-            renderPageEdit = <FormProfessor isEdit={true} defaultValues={item} />;
-            break;
-        case "aluno":
-            renderPageEdit = <FormAluno isEdit={true} defaultValues={item} />;
-            break;
-        case "disciplina":
-            renderPageEdit = <FormDisciplina isEdit={true} defaultValues={item} />;
-            break;
-        case "usuario":
-            renderPageEdit = <FormUsuario isEdit={true} defaultValues={item} />;
-            break;
-    }
+  if (!renderPageEdit) return null;
 
-    if (!renderPageEdit) return null;
+  return (
+    <TableRow>
+      <TableCell className="font-medium">{item.nome}</TableCell>
+      <TableCell className="hidden md:table-cell">{item.email}</TableCell>
+      <TableCell className="hidden md:table-cell">{item.telefone}</TableCell>
+      <TableCell>
+        <div className="flex lg:hidden items-center justify-end">
+          <DropDownMenuCell />
+        </div>
+        <div className="hidden lg:flex items-center justify-end gap-1">
+          <AlertDialogComponent
+            triggerClassName="bg-red-500"
+            triggerIcon="delete"
+            triggerTooltip={`Deletar ${type}`}
+            dialogTitle={`Deletar ${type}`}
+            dialogDescription={`Tem certeza que deseja deletar este ${type}? Essa ação não poderá ser desfeita.`}
+          />
+          <ActionDialog
+            triggerIcon="edit"
+            triggerTooltip={`Alterar Dados ${type}`}
+            triggerClassName="bg-secundaria"
+            dialogTitle={`Editar ${type}`}
+          >
+            {renderPageEdit}
+          </ActionDialog>
 
-    return (
-        <TableRow>
-            <TableCell className="font-medium">{item.nome}</TableCell>
-            <TableCell className="hidden md:table-cell">{item.email}</TableCell>
-            <TableCell className="hidden md:table-cell">{item.telefone}</TableCell>
-            <TableCell>
-                <div className="flex lg:hidden items-center justify-end">
-                    <DropDownMenuCell />
-                </div>
-                <div className="hidden lg:flex items-center justify-end gap-1">
-                    <AlertDialogComponent
-                        triggerClassName="bg-red-500"
-                        triggerIcon="delete"
-                        triggerTooltip={`Deletar ${type}`}
-                        dialogTitle={`Deletar ${type}`}
-                        dialogDescription={`Tem certeza que deseja deletar este ${type}? Essa ação não poderá ser desfeita.`}
-                    />
-                    <ActionDialog
-                        triggerIcon="edit"
-                        triggerTooltip={`Alterar Dados ${type}`}
-                        triggerClassName="bg-secundaria"
-                        dialogTitle={`Editar ${type}`}
-                    >
-                        {renderPageEdit}
-                    </ActionDialog>
-
-                    <ActionDialog
-                        triggerIcon="view"
-                        triggerTooltip={`Ver mais`}
-                        triggerClassName="bg-secundaria1"
-                        dialogTitle={`Visualizar ${type}`}
-                    >
-                        <p>Aqui dentro vão os detalhes do {type}...</p>
-                        {/* Ex: <UserDetails user={...} /> */}
-                    </ActionDialog>
-                </div>
-            </TableCell>
-        </TableRow>
-    );
-}
+          <ActionDialog
+            triggerIcon="view"
+            triggerTooltip={`Ver mais`}
+            triggerClassName="bg-secundaria1"
+            dialogClassName="min-w-[75vw]"
+            dialogTitle={`Visualizar ${type}`}
+          >
+            <ItemView id={item.id} />
+          </ActionDialog>
+        </div>
+      </TableCell>
+    </TableRow>
+  );
+};
---
+++ src/components/gerenciar/itemView.tsx
@@ -0,0 +1,86 @@
+import { usePageType } from "@/context/pageTypeContext";
+import { IconAvatar } from "../cadastrar/iconAvatar";
+import { Card } from "../ui/card";
+import Image from "next/image";
+import { AppButton } from "../shared/app-button";
+
+export const ItemView = () => {
+  const { type } = usePageType();
+
+  return (
+    <div className="container">
+      <Card className="border-0 flex">
+        <div className="flex flex-col mt-10 justify-center items-center">
+          <div className="w-35 h-35 p-4 bg-gray-300 rounded-full md:w-50 md:h-50">
+            <Image
+              src={`/assets/${type}-icon.png`}
+              alt={`${type} avatar`}
+              width={170}
+              height={170}
+              className="rounded-full w-full h-full"
+              priority={true}
+            />
+          </div>
+          <div className="mt-10 font-bold text-2xl">Fulano da Silva Soares</div>
+          <div className="mt-10 font-bold text-xl">Matrícula: 12345</div>
+
+          <AppButton
+            type="button"
+            intent={"done2"}
+            children="Alterar"
+            className="px-10 mt-2 text-xl"
+          />
+        </div>
+
+        <div className="flex w-60vw justify-between m-10">
+          <div>
+            <p className="mx-2 font-bold">Disciplinas em Curso:</p>
+            <Card className="w-100 border-0 mt-5">
+              <div className="w-full bg-gray-200 border border-primaria rounded-md p-3">
+                Geografia
+              </div>
+              <div className="w-full bg-gray-200 border border-primaria rounded-md p-3">
+                História
+              </div>
+              <div className="w-full bg-gray-200 border border-primaria rounded-md p-3">
+                Matemática
+              </div>
+              <div className="w-full bg-gray-200 border border-primaria rounded-md p-3">
+                Português
+              </div>
+              <div className="w-full bg-gray-200 border border-primaria rounded-md p-3">
+                Ciências
+              </div>
+            </Card>
+          </div>
+          <div>
+            <p className="mx-2 font-bold">Aulas de Hoje</p>
+            <Card className="flex flex-row justify-between w-100 bg-gray-200 border border-primaria rounded-md mt-5">
+              <div className="mx-4">
+                <p className="mb-3 font-bold">Horário:</p>
+                <div className="flex flex-col gap-2">
+                  <p>13:00 - 13:40</p>
+                  <p>13:40 - 14:40</p>
+                  <p>14:40 - 15:40</p>
+                  <p>16:00 - 16:40</p>
+                  <p>16:40 - 17:40</p>
+                </div>
+              </div>
+
+              <div className="mx-4">
+                <p className="mb-3 font-bold">Disciplina:</p>
+                <div className="flex flex-col gap-2">
+                  <p>Português</p>
+                  <p>Português</p>
+                  <p>Intervalo</p>
+                  <p>Matemática</p>
+                  <p>Matemática</p>
+                </div>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
